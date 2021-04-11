23015020c8d6ccf9775a874d8110605d5b092712
Andrey <andreybat2@gmail.com>
2021-04-11 12:50:26 +0300

    Домашнее задание 5

--- src/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    public static void main(String[] args) {
-        Point p1 = new Point(4, 4);
-        Point p2 = new Point(5, 7);
-//        double result = p1.getDistance(p2);
-        double result = Point.getDistance(p1, p2);
-        System.out.println(result); // 3.1622776601683795
-    }
-}

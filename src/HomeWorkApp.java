4172e95cc72959a145a161501fab0f3b1ec008cc
Andrey <andreybat2@gmail.com>
2021-04-15 22:13:21 +0300

    Домашнее задание 6

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,13 +1,21 @@
 // Домашнее задание №6
+
 public class HomeWorkApp {
     public static void main(String[] args) {
             AnimalsCounter animCount = new AnimalsCounter();
             Cat cat = animCount.newCat(200);
             Dog dog = animCount.newDog(500, 10);
+
             cat.run(201);
             cat.swim(20);
+
             dog.run(180);
             dog.swim(40);
+
+            Cat cat2 = animCount.newCat(100);
+            cat.run(101);
+            cat.swim(0);
+
             System.out.println("Было вызвано собак: " + animCount.getDogCount());
             System.out.println("Было вызвано кошек: " + animCount.getCatCount());
             System.out.println("Было вызвано животных: " + animCount.getAnimalsCount());

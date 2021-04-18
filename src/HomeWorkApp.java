8f6c8bf4e14c07a8875f7f7f4098ff6a850ab187
Andrey <andreybat2@gmail.com>
2021-04-18 23:01:12 +0300

    Домашнее задание 8

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,4 +1,4 @@
-Домашнее задание №8
+// Домашнее задание №8
 public class HomeWorkApp {
     public static void main(String[] args) {
         // Создаем объект MyWindow и вызываем метод MyWindow в одноименном классе
